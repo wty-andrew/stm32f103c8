d89a06012ef2593ea0ccf3664bd78da89712c435
Andrew <wty.andrew@gmail.com>
2021-07-26 12:53:06 +0800

    :tada: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define led PC13
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);
+}
